b0f60ef4bd82fd77f88eec8eb40795287284f76a
Brian Holland <neumaics@gmail.com>
2016-09-19 22:44:16 -0600

    added basic react system

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const childProcess = require('child_process');
+const electron = require('electron-prebuilt');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default', function() { });
+
+gulp.task('run', ['scripts'], function () {
+  childProcess.spawn(electron, ['.'], { stdio: 'inherit' });
+});
+
+gulp.task('scripts', () => {
+  return gulp.src('app/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dist'));
 });
