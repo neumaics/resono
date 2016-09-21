234612654585dedf1147d21d768226bfde91e28a
Brian Holland <neumaics@gmail.com>
2016-09-20 20:19:10 -0600

    getting less and react infrastructure set up

--- app/app.js
+++ app/app.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-class Layout extends React.Component {
-  render() {
-    return (
-      <h1>Hello react!</h1>
-    );
-  }
-}
+import Layout from './components/layout';
 
-const app = document.getElementById('app');
-
-ReactDom.render(<Layout />, app);
+ReactDom.render(<Layout />, document.getElementById('app'));
--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,31 @@ const childProcess = require('child_process');
 const electron = require('electron-prebuilt');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
+const less = require('gulp-less');
+const path = require('path');
+const clean = require('gulp-clean');
 
 gulp.task('default', function() { });
 
-gulp.task('run', ['scripts'], function () {
+gulp.task('run', ['scripts', 'less'], function () {
   childProcess.spawn(electron, ['.'], { stdio: 'inherit' });
 });
 
-gulp.task('scripts', () => {
+gulp.task('scripts', ['clean'], () => {
   return gulp.src('app/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('less', ['clean'], function () {
+  return gulp.src('./app/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('clean', function () {
+  return gulp.src('dist', { read: false })
+    .pipe(clean());
 });
