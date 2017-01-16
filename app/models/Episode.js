c970e78e3c2fb8b42cbd0074735cadc9cdb0063b
Brian <neumaics@gmail.com>
2017-01-16 11:35:06 -0700

    bug: some episodes don't have enclosure.

--- app/models/Episode.js
+++ app/models/Episode.js
@@ -4,12 +4,14 @@ import moment from 'moment';
 export default class Episode { }
 
 Episode.fromRss = function(rssItem) {
+  const url = rssItem.enclosure ? rssItem.enclosure.$.url : '';
+
   return {
     id: md5(rssItem.title + rssItem.pubDate),
     title: rssItem.title,
     description: rssItem.description,
     pubDate: moment(rssItem.pubDate, "ddd, DD MMM YYYY HH:mm:ss ZZ").utc().valueOf(),
-    url: rssItem.enclosure.$.url,
+    url: url,
     listened: false,
     removed: false
   };
